04a2562616ea1cbba2090e3e73a4948ead87d61c
FedeBugni <bugnifede@gmail.com>
2024-11-30 23:30:19 +0100

    header links are now underlined when clicked

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,25 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import './Header.css'
-import logo from '../../assets/logo.png'
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import './Header.css';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <header className='container'>
-        <Link to='/'><img src={logo} alt="" className='logo'/></Link>
-        <ul>
-          <Link to='/artDirection'><li>Art Direction</li></Link>
-          <Link to='/setDesign'><li>Set Design</li></Link>
-          <Link to='/projects'><li>Projects</li></Link>
-          <Link to='/installation'><li>Installation</li></Link>
-          <Link to='/testeIdra'><li>Teste D'Idra</li></Link>
-          <Link to='/exhibition'><li>Exhibition</li></Link>
-          <Link to='/archive'><li>Archive</li></Link>
-          <Link to='/contacts'><li>Contacts</li></Link>
-          <Link to='/about'><li>About us</li></Link>
-        </ul>
-    </header>
-  )
-}
+    const [activeLink, setActiveLink] = useState('');
 
-export default Header
+    const handleLinkClick = (link) => {
+        setActiveLink(link);
+    };
+
+    const handleLogoClick = () => {
+        setActiveLink('');
+    };
+
+    return (
+        <header className='container'>
+            <Link to='/' onClick={handleLogoClick}><img src={logo} alt="" className='logo'/></Link>
+            <ul>
+                <Link to='/artDirection' onClick={() => handleLinkClick('/artDirection')}>
+                    <li className={activeLink === '/artDirection' ? 'active' : ''}>Art Direction</li>
+                </Link>
+                <Link to='/setDesign' onClick={() => handleLinkClick('/setDesign')}>
+                    <li className={activeLink === '/setDesign' ? 'active' : ''}>Set Design</li>
+                </Link>
+                <Link to='/projects' onClick={() => handleLinkClick('/projects')}>
+                    <li className={activeLink === '/projects' ? 'active' : ''}>Projects</li>
+                </Link>
+                <Link to='/installation' onClick={() => handleLinkClick('/installation')}>
+                    <li className={activeLink === '/installation' ? 'active' : ''}>Installation</li>
+                </Link>
+                <Link to='/testeIdra' onClick={() => handleLinkClick('/testeIdra')}>
+                    <li className={activeLink === '/testeIdra' ? 'active' : ''}>Teste D&#39;Idra</li>
+                </Link>
+                <Link to='/exhibition' onClick={() => handleLinkClick('/exhibition')}>
+                    <li className={activeLink === '/exhibition' ? 'active' : ''}>Exhibition</li>
+                </Link>
+                <Link to='/archive' onClick={() => handleLinkClick('/archive')}>
+                    <li className={activeLink === '/archive' ? 'active' : ''}>Archive</li>
+                </Link>
+                <Link to='/contacts' onClick={() => handleLinkClick('/contacts')}>
+                    <li className={activeLink === '/contacts' ? 'active' : ''}>Contacts</li>
+                </Link>
+                <Link to='/about' onClick={() => handleLinkClick('/about')}>
+                    <li className={activeLink === '/about' ? 'active' : ''}>About us</li>
+                </Link>
+            </ul>
+        </header>
+    );
+};
+
+export default Header;
